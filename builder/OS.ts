58da9bcfd4e89642b5b07546a7b35beb245840da
duart38 <mr.codercuracao@gmail.com>
2021-03-15 11:55:10 +0100

    added documentation

--- builder/OS.ts
+++ builder/OS.ts
@@ -1,4 +1,11 @@
 type OSName = "darwin" | "linux" | "windows";
+
+/**
+ * Deno operating system specific code builder.
+ * @example OS.on("darwin")
+ *            .do(()=>console.log("mac"))
+ *            .else(()=>console.log("not mac"));
+ */
 export class OS {
     private name: OSName;
     private isOS: boolean;
@@ -7,21 +14,51 @@ export class OS {
         this.isOS = Deno.build.os == this.name;
     }
 
+    /**
+     * Do something when the OS name is equals to the last provided name @see on
+     * @example OS.on("darwin").do(()=>console.log("mac"));
+     * @param run the function to run
+     * @returns this
+     */
     public do(run: ()=>void): this {
         if(this.isOS) run();
         return this;
     }
 
+    /**
+     * Do something when the OS name is NOT equals to the last provided name @see on
+     * @example OS.on("darwin")
+     *            .do(()=>console.log("mac"))
+     *            .else(()=>console.log("not mac"));
+     * @param run 
+     * @returns this
+     */
     public else(run: ()=>void): this {
         if(!this.isOS) run();
         return this;
     }
+
+    /**
+     * Updates the working OS name for further chain links.
+     * @example OS.on("darwin")
+     *             .do(()=>console.log("mac code"))
+     *            .on("windows")
+     *             .do(()=>console.log("windows code"));
+     * @param name 
+     * @returns this
+     */
     public on(name: OSName): this{
         this.name = name;
         this.isOS = Deno.build.os == this.name;
         return this;
     }
 
+    /**
+     * Initializes the builder.
+     * @example OS.on("darwin")
+     * @param name name of the operating system to check against
+     * @returns this
+     */
     static on(name: OSName): OS {
         return new OS(name);
     }
