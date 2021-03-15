654f65b9f48d0b7f1826870797761a1823d97bc1
duart38 <mr.codercuracao@gmail.com>
2021-03-15 12:38:44 +0100

    added documentation for ARGS

--- builder/ARGS.ts
+++ builder/ARGS.ts
@@ -1,3 +1,9 @@
+/**
+ * Deno command line argument specific code builder.
+ * @example ARGS.on("-x")
+ *              .do(()=>console.log("-x"))
+ *              .else(()=>console.log("-x not found"));
+ */
 export class ARGS {
     private args: string[];
     private includesAll: boolean;
@@ -6,20 +12,52 @@ export class ARGS {
         this.includesAll = this.args.every((arg)=>Deno.args.includes(arg));
     }
     
+    /**
+     * runs a function when all arguments provided are set
+     * @example ARGS.on("-x")
+     *              .do(()=>console.log("-x"))
+     *              .else(()=>console.log("-x not found"));
+     * @param run function to run
+     * @returns this
+     */
     public do(run: ()=>void): this {
         if(this.includesAll) run();
         return this;
     }
 
+    /**
+     * runs a function when one or more arguments provided are NOT set
+     * @example ARGS.on("-x")
+     *              .do(()=>console.log("-x"))
+     *              .else(()=>console.log("-x not found"));
+     * @param run function to run
+     * @returns this
+     */
     public else(run: ()=>void): this {
         if(!this.includesAll) run();
         return this;
     }
 
+     /**
+     * Helper method to facilitate declarative chaining
+     * @example ARGS.on("-x")
+     *               .do(()=>console.log("-x code"))
+     *              .on("-a")
+     *               .do(()=>console.log("-a code"));
+     * @param name 
+     * @returns new instance of ARGS
+     */
     public on(...argument: string[]): ARGS {
         return new ARGS(argument);
     }
 
+    /**
+     * Helper method to start chaining
+     * @example ARGS.on("-x")
+     *              .do(()=>console.log("-x code"))
+     * @param name 
+     * @returns new instance of ARGS
+     */
     static on(...argument: string[]): ARGS{
         return new ARGS(argument);
     }
