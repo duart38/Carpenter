83306c4ade1c4a58704b1f6ccd8348b711301782
duart38 <mr.codercuracao@gmail.com>
2021-03-15 20:27:55 +0100

    added more tests

--- tests/STRING_test.ts
+++ tests/STRING_test.ts
@@ -28,37 +28,24 @@ Deno.test("STRING has(string) else", () => {
     if(!found) fail("lo not found in hello");
 });
 
-// Deno.test("Runs else clause", () => {
-//   ARGS.on("-notThere")
-//     .do(() => fail("incorrect clause"))
-//     .else(() => assert("else clause."));
-// });
+Deno.test("STRING has(string) and/or simple", () => {
+    let found = false;
+    STRING("hello")
+    .has("h").and().has("xx")
+    .or().has("hello")
+    .do(()=>{
+        assert("Or clause works")
+        found = true;
+    })
+    if(!found) fail("lo not found in hello");
+});
 
-// Deno.test("Runs on chaining request", () => {
-//   ARGS.on("-notThere")
-//     .do(() =>fail("flag clause -notThere reached.."))
-//     .on("-x").do(() => assert("Chaining works.."))
-//     .else(()=>fail("else clause of correct flag reached"));
-// });
 
-// Deno.test("Premature Else works", () => {
-//       ARGS.on("-notThere").else(()=>assert("Works"));
-// });
 
-// Deno.test("Multiple do clauses", () => {
-//     let str = "";
-//       ARGS.on("-x")
-//         .do(()=>str += "Hello ")
-//         .do(()=>str += "world");
-//     assertStringIncludes(str, "Hello world");
-// });
-
-// Deno.test("Multiple do clauses", () => {
-//   let t;
-//     ARGS.on("-x", "-a")
-//         .do(()=>{
-//           t = true; 
-//           assert("found -x and -a");
-//         })
-//   if(!t) fail("")
-// });
+Deno.test("STRING Multiple do clauses", () => {
+    let str = "";
+      STRING("x").has("x")
+        .do(()=>str += "Hello ")
+        .do(()=>str += "world");
+    assertStringIncludes(str, "Hello world");
+});
