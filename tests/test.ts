ed722bb91af1b5f367fd1de31a9779e813f9316c
duart38 <mr.codercuracao@gmail.com>
2021-03-15 11:38:40 +0100

    add more tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,19 +1,40 @@
 import { assert, fail } from "https://deno.land/std@0.90.0/testing/asserts.ts";
-import {OS} from "../mod.ts";
+import { OS } from "../mod.ts";
 
 Deno.test("Runs on target request", () => {
-    OS.on(Deno.build.os).do(()=>{
-        assert(Deno.build.os);
-    }).else(()=>{
-        fail(`Calling else clause for os ${Deno.build.os} on request for ${Deno.build.os}`);
+  OS.on(Deno.build.os)
+    .do(() => {
+      assert(Deno.build.os);
     })
+    .else(() => {
+      fail(
+        `Calling else clause for os ${Deno.build.os} on request for ${Deno.build.os}`
+      );
+    });
 });
 
 Deno.test("Runs else clause", () => {
-    const notThis = Deno.build.os == "darwin" ? "windows" : "darwin";
-    OS.on(notThis).do(()=>{
-        fail(`Calling do clause for os ${Deno.build.os} on request for ${notThis}`);
-    }).else(()=>{
-        assert("else clause.")
+  const notThis = Deno.build.os == "darwin" ? "windows" : "darwin";
+  OS.on(notThis)
+    .do(() => {
+      fail(
+        `Calling do clause for os ${Deno.build.os} on request for ${notThis}`
+      );
     })
+    .else(() => {
+      assert("else clause.");
+    });
+});
+
+Deno.test("Runs on chaining request", () => {
+const notThis = Deno.build.os == "darwin" ? "windows" : "darwin";
+  OS.on(notThis)
+    .do(() =>fail("Target build is supposed to be windows but linux clause was run"))
+    .on(Deno.build.os).do(() => assert("Chaining works.."))
+    .else(()=>fail("else clause ran on chaining of valid OS"));
+});
+
+Deno.test("Premature Else works", () => {
+    const notThis = Deno.build.os == "darwin" ? "windows" : "darwin";
+      OS.on(notThis).else(()=>assert("Works"))
 });
