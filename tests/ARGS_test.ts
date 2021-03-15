ab9c9b752e7e4cb3c99af528af2dbbcd6c103d84
duart38 <mr.codercuracao@gmail.com>
2021-03-15 19:37:59 +0100

    Changed duplicate test names

--- tests/ARGS_test.ts
+++ tests/ARGS_test.ts
@@ -3,7 +3,7 @@ import { ARGS } from "../mod.ts";
 
 // run these tests with -x and -a as an argument (deno test -- -x -a)
 
-Deno.test("Runs on target request", () => {
+Deno.test("Runs on -x supplied", () => {
     let found = false;
     ARGS.on("-x").do(() => {
       assert("found -x");
@@ -12,24 +12,24 @@ Deno.test("Runs on target request", () => {
     if(!found) fail("-x not found");
 });
 
-Deno.test("Runs else clause", () => {
+Deno.test("Runs else clause when flag not available", () => {
   ARGS.on("-notThere")
     .do(() => fail("incorrect clause"))
     .else(() => assert("else clause."));
 });
 
-Deno.test("Runs on chaining request", () => {
+Deno.test("Runs second item in chain", () => {
   ARGS.on("-notThere")
     .do(() =>fail("flag clause -notThere reached.."))
     .on("-x").do(() => assert("Chaining works.."))
     .else(()=>fail("else clause of correct flag reached"));
 });
 
-Deno.test("Premature Else works", () => {
+Deno.test("Premature Else on non existent flag works", () => {
       ARGS.on("-notThere").else(()=>assert("Works"));
 });
 
-Deno.test("Multiple do clauses", () => {
+Deno.test("Multiple do clauses for -x flag", () => {
     let str = "";
       ARGS.on("-x")
         .do(()=>str += "Hello ")
@@ -37,7 +37,7 @@ Deno.test("Multiple do clauses", () => {
     assertStringIncludes(str, "Hello world");
 });
 
-Deno.test("Multiple do clauses", () => {
+Deno.test("Unions of flags both supplied", () => {
   let t;
     ARGS.on("-x", "-a")
         .do(()=>{
@@ -46,4 +46,14 @@ Deno.test("Multiple do clauses", () => {
         })
   if(!t) fail("")
 });
-// todo add more flags in "on"
+
+Deno.test("Unions of flags only one supplied", () => {
+  let t;
+    ARGS.on("-x", "-notThere")
+        .do(()=>{})
+        .else(()=>{
+          t = true; 
+          assert("found -x and -a");
+        })
+  if(!t) fail("")
+});
