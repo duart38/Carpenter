658d115e549f31d4d12e8ed9c35cb5ea622a1b93
duart38 <mr.codercuracao@gmail.com>
2021-03-16 10:35:17 +0100

    changed export type, added new builder type

--- types/Builder.ts
+++ types/Builder.ts
@@ -1,4 +1,9 @@
-export default interface Builder<I> {
+export interface Builder<I> {
   do: (run: () => void) => I;
   else: (run: () => void) => I;
+}
+
+export interface WritableBuilder<A, B> {
+  do: (run: () => void | A) => B;
+  else: (run: () => void | A) => B;
 }
