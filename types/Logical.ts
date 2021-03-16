c7b182aa4a156cf91f97b0a344ee05ff89b99b24
duart38 <mr.codercuracao@gmail.com>
2021-03-16 11:21:47 +0100

    jdoc

--- types/Logical.ts
+++ types/Logical.ts
@@ -3,6 +3,14 @@ export enum LOGICAL {
     OR,
 }
 export interface Logical {
+    /**
+     * Results in the next call having a logical AND (&&) applied to the result of the last check.
+     * @returns itself
+     */
     and(): this;
+    /**
+     * Results in the next call having a logical OR (||) applied to the result of the last check.
+     * @returns itself
+     */
     or(): this;
 }
