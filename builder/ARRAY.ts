8ce65fd9f03f3bd0e5297f76ead8966474eaab7b
duart38 <mr.codercuracao@gmail.com>
2021-03-16 11:01:07 +0100

    added the basic array methods

--- builder/ARRAY.ts
+++ builder/ARRAY.ts
@@ -1,9 +1,64 @@
-export class ARR<I> {
+import {WritableBuilder} from "../types/Builder.ts";
+import {LOGICAL} from "../types/Logical.ts";
+
+export class ARR<I> implements WritableBuilder<Array<I>, ARR<I>>{
     private value: I[]
+    private lastStackMatched: boolean;
+    private previousLogical: LOGICAL;
     constructor(val: I[]){
         this.value = val;
+        this.lastStackMatched = true;
+        this.previousLogical = LOGICAL.AND;
+    }
+    private computeWithPrevious(onCurrent: boolean): boolean {
+        switch (this.previousLogical) {
+          case LOGICAL.AND:
+            return this.lastStackMatched && onCurrent;
+          case LOGICAL.OR:
+            return this.lastStackMatched || onCurrent;
+        }
     }
 
+    /**
+     * Appends regardless of result of the previous chain items.
+     * @param toAdd 
+     */
+    public append(...toAdd: I[]): this{
+        this.value.push(...toAdd);
+        return this;
+    }
+
+    /**
+     * Appends only if the previous chain items result in a "true".
+     * @param toAdd 
+     */
+    public thenAppend(...toAdd: I[]): this{
+        if(this.lastStackMatched) this.value.push(...toAdd);
+        return this;
+    }
+
+    /**
+     * Checks if the current value is of size 'n'
+     * @param n 
+     */
+    public isOfSize(n: number): this{
+        this.lastStackMatched = this.computeWithPrevious(this.value.length == n);
+        return this;
+    }
+
+    public do(run: (currentValue: I[]) => void|I[]): this{
+        if(this.lastStackMatched){
+            this.value = run(this.value) || this.value;
+        }
+        return this;
+    }
+
+    public else(run: (currentValue: I[]) => void|I[]): this{
+        if(!this.lastStackMatched){
+            this.value = run(this.value) || this.value;
+        }
+        return this;
+    }
     
 
 }
