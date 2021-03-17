8365f8ca6d71ea7209e17e5def5c0aa083faf4ae
duart38 <mr.codercuracao@gmail.com>
2021-03-17 11:11:36 +0100

    add export for new carpenters

--- mod.ts
+++ mod.ts
@@ -3,3 +3,5 @@ export { ARGS } from "./builder/ARGS.ts";
 export { STR, STRING } from "./builder/STRING.ts";
 export { ARR, ARRAY } from "./builder/ARRAY.ts";
 export { SWTC, SWITCH } from "./builder/SWITCH.ts";
+export { OBJECT, OBJ } from "./builder/OBJECT.ts";
+export { VALUE, VAL } from "./builder/VALUE.ts";
